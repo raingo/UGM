1e540b1d066772a9df1c2a013e0640a0081f7b88
Yuncheng Li <raingomm@gmail.com>
2017-06-09 16:14:57 -0700

    include eta to fmin

--- fmin_sgd.m
+++ fmin_sgd.m
@@ -1,10 +1,12 @@
 function w = fmin_sgd(func, w0, iters, stepSize0)
   w = w0;
+  tic;
   for iter = 1:iters
     [f,g] = func(w, iter);
     stepSize = stepSize0 * min(1000.0/double(iter), 1.0);
     if mod(iter, 10) == 0
-      fprintf('Iter = %d of %d (fsub = %f) (lr = %f)\n',iter, iters, f, stepSize);
+      eta = toc / iter * (iters - iter);
+      fprintf('Iter = %d of %d (fsub = %.3g) (lr = %.3g) (eta = %.3f)\n',iter, iters, f, stepSize, eta);
     end
     w = w - stepSize*g;
   end
