91d492a5df7472fdf6d68c4c75730addf5d7af47
Yuncheng Li <raingomm@gmail.com>
2017-06-09 15:45:05 -0700

    less echo

--- fmin_sgd.m
+++ fmin_sgd.m
@@ -1,8 +1,10 @@
 function w = fmin_sgd(func, w0, iters, stepSize0)
-w = w0;
-for iter = 1:iters
+  w = w0;
+  for iter = 1:iters
     [f,g] = func(w, iter);
     stepSize = stepSize0 * min(1000.0/double(iter), 1.0);
-    fprintf('Iter = %d of %d (fsub = %f) (lr = %f)\n',iter, iters, f, stepSize);
+    if mod(iter, 10) == 0
+      fprintf('Iter = %d of %d (fsub = %f) (lr = %f)\n',iter, iters, f, stepSize);
+    end
     w = w - stepSize*g;
-end
+  end
