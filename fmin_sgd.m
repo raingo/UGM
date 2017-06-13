951137b42456497e34b180d177f85531e57891c4
Yuncheng Li <raingomm@gmail.com>
2017-06-13 13:26:13 -0700

    nan detect

--- fmin_sgd.m
+++ fmin_sgd.m
@@ -3,6 +3,10 @@ function w = fmin_sgd(func, w0, iters, stepSize0)
   tic;
   for iter = 1:iters
     [f,g] = func(w, iter);
+    if any(isnan(g))
+      fprintf('%d\n', iter);
+      error('NaN found');
+    end
     iter = double(iter);
     stepSize = stepSize0 * min(1000.0/iter, 1.0);
     if mod(iter, 10) == 0 || iter == 1
